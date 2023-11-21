77f3393489d8540909ebdba5e3586fd822f3e493
codephenix4 <yahia19raouf@gmail.com>
2023-11-21 12:59:46 +0100

    added social network image

---
+++ src/app/components/Reseau.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import resaeu from 'public/reseau.png'
+
+const Reseau = () => {
+  return (
+    <Image  
+    src={resaeu}
+    alt="background image"
+    width={200}
+    height={200}
+    className="relative cursor-pointer rounded-2xl transition ease-in-out p-1  hover:scale-110
+    focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 
+    focus:ring-offset-gray-800 	mb-4 "
+     />
+  
+  
+  )
+}
+
+export default Reseau
