598b5f5af54d9b64c6b2489ac833f770ed16409a
raouf <yahia19raouf@gmail.com>
2023-12-01 22:49:05 +0100

    create a  fonction to display the rating

--- src/app/components/AvisClients.tsx
+++ src/app/components/AvisClients.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  { avisClients }  from "../../../util/getAvisClients"
 import Image from 'next/image';
 import pene from "../../../public/pene.jpeg"
@@ -14,55 +14,39 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
   
 
 function AvisClients() {
-
     const avis = avisClients();
-   
-
   return (
 
-    <section className='flex flex-col items-center gap-8 p-8 bg-green-100'>
-        <h2 className='text-2xl'> Ce que disent nos clients</h2>
-
-   
-
-    
+    <section className='flex flex-col items-center  gap-8 p-8 bg-green-100'>
+        <h2 className='text-2xl my-8'> Ce que disent nos clients</h2>
         <section  className='flex flex-raw gap-6'> 
-     
         {avis.map((item,index)=>{
+             let rating = item.rating;
+             let ratingNu  = <FontAwesomeIcon  icon={fas.faStar } className=' w-3 text-yellow-400 '/>
             return (
-           //  <li key={index}>{item}</li>
-          
             <div key={index} className=" flex flex-col items-center justify-center gap-2  max-w-[25%]">
             <Image  src={"/"+ item.image} width={120} height="150" alt="burger" />
-            <h6> {item.rating}  <FontAwesomeIcon  icon={fas.faStar } className=' w-3 text-yellow-400 '/></h6> 
+          <ul className='flex flex-raw gap-1 '>
+              {[...Array(rating)].map((_, i)=> (
+              <li key={i}>{ratingNu}</li>
+              ))}
+          </ul>
             <h4 className="  font-serif hover:font-sans  text-black text-lg" key={index}>{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</h4>
             <p className="  font-sans  text-black text-sm w-fit">{item.avis}</p>
-          
-            
-
           </div>
-         
             )
         })}
-         
          </section>
-
          <section className='flex flex-raw  w-full'>
             <div className='flex flex-col bg-gradient-to-r from-purple-600 to-orange-600 w-1/2 justify-center items-center justify-around'>
                 <h3 className='text-white'> Notre objectif est de vous satisfaire</h3>
                 <h6 className='text-white'> Chaque avis compte!</h6>
-
                 <button className='bg-black p-4 text-xl text-white' > Rédiger un commentaire </button> 
-
             </div>
             <div className='w-1/2'>
                 <Image src={pene}  width={1200} alt="spagiti" />
             </div>
-
          </section>
-    
-
-
     </section>
   )
 }
--- util/getAvisClients.ts
+++ util/getAvisClients.ts
@@ -1,3 +1,4 @@
+import { LocaleRouteNormalizer } from "next/dist/server/future/normalizers/locale-route-normalizer";
 
 export const avisClients = () => {
     // const response = await delay(1000)
@@ -6,26 +7,26 @@ export const avisClients = () => {
             name:"paul",
             image: "paul.jpeg",
             avis : "Love the design and customization of InstaShow 👍. We have used various Instagram apps for Shopify in the past but they would always mess up our theme. I love how we",
-            rating: 5
+            rating: 4
         },
         {
             name : "mark",
             image : "mark.jpeg" ,
-            avis : "Love the design and customization of InstaShow 👍. We have used various Instagram apps for Shopify in the past but they would always mess up our theme. I love how we",
-            rating : 4
+            avis : "La présence d’avis moins positifs est aussi un gage d’authenticité, à l’heure où les consommateurs demandent plus de transparence de la part des marques",
+            rating : 5
         },
         {
             name : "steven",
             image : "steven.jpeg"    ,
-            avis : "Love the design and customization of InstaShow 👍. We have used various Instagram apps for Shopify in the past but they would always mess up our theme. I love how we",
+            avis : "Les consommateurs apprécient donner leurs avis pour aider d’autres acheteurs.Quelques avis publiés suffisent à booster vos performances commerciales et à donner confiance aux consommateurs",
             rating : 4
         
         },
         {
             name : "sylvie",
             image : "sylvie.jpeg",
-            avis : "Love the design and customization of InstaShow 👍. We have used various Instagram apps for Shopify in the past but they would always mess up our theme. I love how we",
-            rating: 4
+            avis : "Nous avons pensé notre produit pour vous rendre autonome dans l’implémentation, sans surcharger vos équipes.Bien sûr, nous ne sommes jamais très loin pour vous accompagner dans le pilotage du projet.",
+            rating: 5
 
         }
     ];
