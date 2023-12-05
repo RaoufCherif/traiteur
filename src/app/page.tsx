a6cb068839ef474fcf1b0788a1abaa4385cbc46f
raouf <yahia19raouf@gmail.com>
2023-12-05 22:14:10 +0100

    solved git merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,28 +23,21 @@ export default function Home() {
 
             <div id="accueil_section_1" className=" flex flex-col  justify-around  items-center  ">
               <div id="titre_traiteur" className="flex  flex-col items-center ">
-<<<<<<< HEAD
-              <h1 className="relative font-serif text-purple-500 hover:font-sans text-7xl hover:scale-110 mt-80  ">Traiteur Parisien </h1>
-              <h6 className="relative font-serif text-purple-300" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi officiis vitae quod omnis earum.</h6>
-=======
+
               <h1 className="relative font-serif text-purple-500 hover:font-sans text-7xl hover:scale-110 mt-44  ">Traiteur Parisien </h1>
               <h6 className="relative font-serif  text-purple-200 text-xl" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi officiis vitae quod omnis earum.</h6>
->>>>>>> 5de0a80 (readme)
+
             
           </div>
           <div className="flex flex-col items-center">
 
 
         <Link href="/dashboard">
-<<<<<<< HEAD
-          <h4 id="gagner_repas" className="relative font-serif text-purple-700  hover:font-sans   text-xl hover:scale-110 mb-2 mt-28 ">
-            Suivez-nous pour gagner un repas!
-          </h4>
-=======
+
           <h1 id="gagner_repas" className="relative font-serif text-purple-500  hover:font-sans   text-6xl hover:scale-110 mb-4 mt-28 ">
             Gagner un repas!
           </h1>
->>>>>>> 5de0a80 (readme)
+
         </Link>
         <Reseau />
 
