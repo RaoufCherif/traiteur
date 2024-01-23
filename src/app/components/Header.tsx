18191a7e520f47c5b0aaba39dc037ff9b6a22a02
RaoufCherif <124141848+RaoufCherif@users.noreply.github.com>
2024-01-23 20:02:03 +0100

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -119,7 +119,7 @@ const Header = () => {
    
 
 
-        { status === "authenticated" ?     <> <div  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"> {session?.user.email } </div>   <LogOutButton /> </>  : <LogInButton /> }  
+        { status === "authenticated" ? <> <div  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"> {session?.user.email } </div>   <LogOutButton /> </>  : <LogInButton /> }  
             </div>
           </div>
         </div>
