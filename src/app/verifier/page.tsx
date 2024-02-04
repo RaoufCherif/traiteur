86f4d8260398345a27c2b52c03d00cd522997306
raouf@raouf-Inspiron-16-5620 <yahia19raouf@gmail.com>
2024-02-04 22:38:46 +0100

    message afficher après inscription

--- src/app/verifier/page.tsx
+++ src/app/verifier/page.tsx
@@ -4,8 +4,7 @@ import React from 'react'
 function InscriptionMessage() {
   return (
     <div className='h-screen flex flex-col gap-5 justify-center items-center '>
-        <h1 className='bg-amber-300 p-4 rounded-lg'> Vous allez recevoir un mail de confirmation, merci de confirmer l'email que vous avez utiliser pour vous s'inscire !  </h1>
-        <p>Si vous n'êtes pas à l'origine de cette inscription merci d'ignorer ce message !  </p>
+        <h1 className='bg-amber-300 p-4 rounded-lg'> Un e-mail de confirmation vous a été envoyé. Veuillez cliquer sur le lien dans l&apos;e-mail pour confirmer votre inscription.</h1>
     </div>
   )
 }
