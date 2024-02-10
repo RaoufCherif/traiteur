c728ae614916fd2c4229fd68000b7c15ba83eaa3
raouf@raouf-Inspiron-16-5620 <yahia19raouf@gmail.com>
2024-02-10 22:50:49 +0100

    added traditionnel section

--- src/app/components/Patisserie.tsx
+++ src/app/components/Patisserie.tsx
@@ -6,75 +6,75 @@ import { fetchData } from '../../../util/getCakes';
 
 
 function Patisserie() {
-   let data = fetchData();
+  let data = fetchData();
   return (
-<main className=" bg-gradient-to-t from-green-300 via-amber-400 to-green-300  flex flex-col ">
+    <main className=" bg-gradient-to-t from-green-300 via-amber-400 to-green-300  flex flex-col ">
 
-    <section className="h-screen bg-sky-100 flex flex-col  items-center  bg-[url('../../public/beautiful_cakes_ia.png')] bg-no-repeat bg-cover  " >
+      <section className="h-screen bg-sky-100 flex flex-col  items-center  bg-[url('../../public/beautiful_cakes_ia.png')] bg-no-repeat bg-cover  " >
         <div>
-        <h2 className=' font-serif hover:font-sans text-green-700 text-4xl  mt-48 '> Nos Patisseries Faites Maison</h2>
+          <h2 className=' font-serif hover:font-sans text-green-700 text-4xl  mt-48 '> Nos Patisseries Faites Maison</h2>
         </div>
-     
-        </section>
-        <section>
 
-<div>
-<div className=" mt-12 flex flex-col  items-center">
-<h2 className=" font-serif hover:font-sans  text-white text-4xl ">
-Offrez-vous une expérience pâtissière unique
-</h2>
-<p className="mb-20  font-serif hover:font-sans  text-white  ">
-Un gâteau exceptionnel, pour une expérience inoubliable
-</p>
+      </section>
+      <section>
 
-<div className="flex flex-raw gap-10 mb-20">
-{data.map((item, index) => {
-return (
-//  <li key={index}>{item}</li>
+        <div>
+          <div className=" mt-12 flex flex-col  items-center">
+            <h2 className=" font-serif hover:font-sans  text-white text-4xl ">
+              Offrez-vous une expérience pâtissière unique
+            </h2>
+            <p className="mb-20  font-serif hover:font-sans  text-white  ">
+              Un gâteau exceptionnel, pour une expérience inoubliable
+            </p>
 
-<div key={index} className=" flex flex-col items-center gap-4 ">
-  <h4 className="  font-serif hover:font-sans  text-white text-2xl">
-    {item.name}
-  </h4>
-  <Image
-    src={"/" + item.image}
-    width="280"
-    height="150"
-    alt="burger"
-    className='rounded-xl'
-  />
-</div>
-);
-})}
+            <div className="flex flex-raw gap-10 mb-20">
+              {data.map((item, index) => {
+                return (
+                  //  <li key={index}>{item}</li>
 
-
+                  <div key={index} className=" flex flex-col items-center gap-4 ">
+                    <h4 className="  font-serif hover:font-sans  text-white text-2xl">
+                      {item.name}
+                    </h4>
+                    <Image
+                      src={"/" + item.image}
+                      width="280"
+                      height="150"
+                      alt="burger"
+                      className='rounded-xl'
+                    />
+                  </div>
+                );
+              })}
 
 
-</div>
 
-<div className="  flex flex-col  items-center justify-bottom">
-  <h6 className=" font-serif hover:font-sans  text-white text-4xl ">
-    35€ pour 40 pièces
-  </h6>
-  <p className="mb-4  font-serif hover:font-sans text-white ">
-    8 pièces de chaque types
-  </p>
-  </div>
 
-<button
-className={clsx(
-"rounded p-4 mb-10 ",
-" bg-gradient-to-r from-green-400 via-green-500 to-green-500",
-"text-white",
-"hover:bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-green-700 to-amber-400",
-)}
->
-Commander 
-</button>
-</div>
-</div>
-</section>
-     
+            </div>
+
+            <div className="  flex flex-col  items-center justify-bottom">
+              <h6 className=" font-serif hover:font-sans  text-white text-4xl ">
+                35€ pour 40 pièces
+              </h6>
+              <p className="mb-4  font-serif hover:font-sans text-white ">
+                8 pièces de chaque types
+              </p>
+            </div>
+
+            <button
+              className={clsx(
+                "rounded p-4 mb-10 ",
+                " bg-gradient-to-r from-green-400 via-green-500 to-green-500",
+                "text-white",
+                "hover:bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-green-700 to-amber-400",
+              )}
+            >
+              Commander
+            </button>
+          </div>
+        </div>
+      </section>
+
     </main>
 
   )
--- src/app/components/TraditionNouveaute.tsx
+++ src/app/components/TraditionNouveaute.tsx
@@ -3,57 +3,67 @@ import Image from "next/image";
 import palm from "../../../public/palm.jpg";
 import snack from "../../../public/mixt_sale_ia.png";
 import tagine from "../../../public/tagine.png";
+import couscous from "../../../public/couscous_poulet_ia.png"
+import clsx from "clsx";
 
 function TraditionNouveaute() {
   return (
-    <main className="">
-          {/* <section className="h-screen bg-sky-100 flex flex-col  items-center  bg-[url('../../public/tagine.png')] bg-no-repeat bg-cover  min-h-full" >
-     </section> */}
-
- 
-      <section   className="  flex flex-col justify-center items-center  "
-      id="traditionnel">
-
-<div>
-        <h2 className=' font-serif hover:font-sans text-green-700 text-4xl  mt-8 '>   Traditionnel</h2>
-        </div>
-      <div className="mb-16 flex flex-row max-w-4xl">
-        <Image
-          id="image_rice"
-          src={palm}
-          alt="background image"
-          className=" w-72 rounded-xl "
-        />
-        <Image
-          src={snack}
-          alt="background image"
-          className="absolute w-1/6 h-1/4 rounded-xl ml-12 mt-24"
-        />
-        <div>
-          <div className="flex flex-col  items-center  gap-4 ml-32 p-4 ">
-            <h2 className="z-50 text-green-700">HMISS & GALETTE</h2>
-            <br />
-            <p className="z-50 text-green-700">
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              molestias id exercitationem? Officiis, error dolores libero
-              accusantium voluptates laboriosam dignissimos, blanditiis quae
-              tempore sapiente magni! Molestias rerum exercitationem repellat
-              obcaecati?
-            </p>
-            <br />
-            <h6 className="text-green-800 text-xl">
-              10€ pour une portion de Hmiss & une galette{" "}
-            </h6>
-          </div>
-        </div>
-      </div>
-      </section>
+    <main className=" ">
 
       <section className="h-screen bg-sky-100 flex flex-col  items-center  bg-[url('../../public/chef_ia.png')] bg-no-repeat bg-cover  min-h-full" >
-     
-     
-     </section>
+
+      </section>
+
+      <section className=" flex flex-col  gap-4   bg-gradient-to-b from-amber-100 via-yellow-100 to-green-200 " id="traditionnel">
+
+
+        <h2 className='flex justify-center text-green-800 font-serif hover:font-sans  text-4xl py-16 '> Traditionnel</h2>
+
+        <div className=" flex flex-raw justify-center justify-around  ">
+
+          <div className="w-1/3 flex flex-col gap-4 justify-center">
+
+            <h6 className="flex justify-center text-slate-900 text-2xl ">Couscous</h6>
+            <Image
+              src={couscous}
+              alt="background image"
+              className="  mb-24 border  border-green-800 p-1 bg-gradient-to-b from-green-400 via-yellow-300 to-green-700"
+
+
+            />
+
+          </div>
+          <div className="w-1/3 flex flex-col gap-4 justify-center">
+
+            <h6 className="flex justify-center text-slate-900 text-2xl ">Tagine</h6>
+            <Image
+
+              src={tagine}
+              alt="background image"
+              className="  mb-24 border  border-green-800 p-1 bg-gradient-to-b from-green-400 via-yellow-300 to-green-700"
+
+
+            />
+
+          </div>
+        </div>
+        <div className="flex justify-center">
+        <button
+              className={clsx(
+                "rounded p-4 mb-10 ",
+                " bg-gradient-to-r from-green-400 via-green-500 to-green-500",
+                "text-white",
+                "hover:bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-green-700 to-amber-400",
+              )}
+            >
+              Commander
+            </button>
+
+            </div>
+      </section>
+
+
+
 
     </main>
   );
